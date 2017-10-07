9923637585a4b417d09126fd5a0ec6e040772a22
Jaya <jaya.bhargavi.kr@sap.com>
2017-10-07 13:31:19 +0530

    Change-Id: Ia44d2deda261e397a0558041558c461511f99610 Signed-off-by: Jaya <jaya.bhargavi.kr@sap.com>

---
+++ src/main/java/com/sap/pm/entity/MetricThresholdConfig.java
@@ -0,0 +1,126 @@
+package com.sap.pm.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "CPU_METRIC_ORIGINAL", schema = "PM_USER")
+@IdClass(MetricThresholdConfigID.class)
+public class MetricThresholdConfig {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) 
+	@Column(name = "ID")
+	private int Id;
+	
+	@Id
+	@Column(name = "METRIC_NAME")
+	private String metricName;
+	
+	@Id
+	@Column(name = "APPLICATION_ID")
+	private String applicationId;
+
+
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+
+	@Column(name = "THRESHOLD")
+	private String threshold;
+	
+
+	@Column(name = "METRC_RUN_NEXT_TIME")
+	private Date metricNextRunTime;
+	
+	@Column(name = "RUN_INTERVAL")
+	private int runInterval;
+	
+	@Column(name = "IS_ALERT_SUPPRESED")
+	private boolean isAlertSuppresed;
+	
+	@Column(name = "IS_ACTIVE")
+	private boolean isActive;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getMetricName() {
+		return metricName;
+	}
+
+	public void setMetricName(String metricName) {
+		this.metricName = metricName;
+	}
+
+	public String getApplicationId() {
+		return applicationId;
+	}
+
+	public void setApplicationId(String applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(String threshold) {
+		this.threshold = threshold;
+	}
+
+	public Date getMetricNextRunTime() {
+		return metricNextRunTime;
+	}
+
+	public void setMetricNextRunTime(Date metricNextRunTime) {
+		this.metricNextRunTime = metricNextRunTime;
+	}
+
+	public int getRunInterval() {
+		return runInterval;
+	}
+
+	public void setRunInterval(int runInterval) {
+		this.runInterval = runInterval;
+	}
+
+	public boolean isAlertSuppresed() {
+		return isAlertSuppresed;
+	}
+
+	public void setAlertSuppresed(boolean isAlertSuppresed) {
+		this.isAlertSuppresed = isAlertSuppresed;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+
+}
---
+++ src/main/java/com/sap/pm/entity/MetricThresholdConfigID.java
@@ -0,0 +1,36 @@
+package com.sap.pm.entity;
+
+import java.io.Serializable;
+
+public class MetricThresholdConfigID implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int Id;
+	private String metricName;
+	private String applicationId;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getMetricName() {
+		return metricName;
+	}
+	public void setMetricName(String metricName) {
+		this.metricName = metricName;
+	}
+	public String getApplicationId() {
+		return applicationId;
+	}
+	public void setApplicationId(String applicationId) {
+		this.applicationId = applicationId;
+	}
+	
+	
+
+}
