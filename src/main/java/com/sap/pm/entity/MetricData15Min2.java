dc11447a2289ddcd70064833cfdd1c79a878d662
Sandeep Mekala <sandeep.mekala@sap.com>
2017-10-08 13:38:42 +0530

    adding entity attributes
    Change-Id: I4ab161647fbdfde09b263c8a415bdd3aa96daa2e
    Signed-off-by: Sandeep Mekala <sandeep.mekala@sap.com>

--- src/main/java/com/sap/pm/entity/MetricData15Min2.java
+++ src/main/java/com/sap/pm/entity/MetricData15Min2.java
@@ -25,11 +25,38 @@ public class MetricData15Min2 {
 	@Column(name = "CPU_USAGE", columnDefinition = "decimal(16,6)")
 	private double cpuUsage;
 	
+	@Column(name = "CPU_USAGE_FORECAST", columnDefinition = "decimal(16,6)")
+	private double cpuUsageForecast;
+	
 	@Column(name = "CPU_USAGE_ENSEMBLE_FORECAST", columnDefinition = "decimal(16,6)")
 	private double cpuUsageEnsembleForecast;
 	
 	@Column(name = "CPU_USAGE_MODEL_MAPE", columnDefinition = "decimal(16,6)")
 	private double cpuUsageMape;
+	
+	@Column(name = "RAM_USAGE", columnDefinition = "decimal(16,6)")
+	private double ramUsage;
+	
+	@Column(name = "RAM_USAGE_FORECAST", columnDefinition = "decimal(16,6)")
+	private double ramUsageForecast;
+	
+	@Column(name = "RAM_USAGE_ENSEMBLE_FORECAST", columnDefinition = "decimal(16,6)")
+	private double ramUsageEnsembleForecast;
+	
+	@Column(name = "RAM_USAGE_MODEL_MAPE", columnDefinition = "decimal(16,6)")
+	private double ramUsageMape;
+	
+	@Column(name = "DISK_USAGE", columnDefinition = "decimal(16,6)")
+	private double diskUsage;
+	
+	@Column(name = "DISK_USAGE_FORECAST", columnDefinition = "decimal(16,6)")
+	private double diskUsageForecast;
+	
+	@Column(name = "DISK_USAGE_ENSEMBLE_FORECAST", columnDefinition = "decimal(16,6)")
+	private double diskUsageEnsembleForecast;
+	
+	@Column(name = "DISK_USAGE_MODEL_MAPE", columnDefinition = "decimal(16,6)")
+	private double diskUsageMape;
 
 	public Date getDate() {
 		return date;
@@ -71,5 +98,76 @@ public class MetricData15Min2 {
 		this.cpuUsageMape = cpuUsageMape;
 	}
 
+	public double getCpuUsageForecast() {
+		return cpuUsageForecast;
+	}
+
+	public void setCpuUsageForecast(double cpuUsageForecast) {
+		this.cpuUsageForecast = cpuUsageForecast;
+	}
+
+	public double getRamUsage() {
+		return ramUsage;
+	}
+
+	public void setRamUsage(double ramUsage) {
+		this.ramUsage = ramUsage;
+	}
+
+	public double getRamUsageForecast() {
+		return ramUsageForecast;
+	}
+
+	public void setRamUsageForecast(double ramUsageForecast) {
+		this.ramUsageForecast = ramUsageForecast;
+	}
+
+	public double getRamUsageEnsembleForecast() {
+		return ramUsageEnsembleForecast;
+	}
+
+	public void setRamUsageEnsembleForecast(double ramUsageEnsembleForecast) {
+		this.ramUsageEnsembleForecast = ramUsageEnsembleForecast;
+	}
+
+	public double getRamUsageMape() {
+		return ramUsageMape;
+	}
+
+	public void setRamUsageMape(double ramUsageMape) {
+		this.ramUsageMape = ramUsageMape;
+	}
+
+	public double getDiskUsage() {
+		return diskUsage;
+	}
+
+	public void setDiskUsage(double diskUsage) {
+		this.diskUsage = diskUsage;
+	}
+
+	public double getDiskUsageForecast() {
+		return diskUsageForecast;
+	}
+
+	public void setDiskUsageForecast(double diskUsageForecast) {
+		this.diskUsageForecast = diskUsageForecast;
+	}
+
+	public double getDiskUsageEnsembleForecast() {
+		return diskUsageEnsembleForecast;
+	}
+
+	public void setDiskUsageEnsembleForecast(double diskUsageEnsembleForecast) {
+		this.diskUsageEnsembleForecast = diskUsageEnsembleForecast;
+	}
+
+	public double getDiskUsageMape() {
+		return diskUsageMape;
+	}
+
+	public void setDiskUsageMape(double diskUsageMape) {
+		this.diskUsageMape = diskUsageMape;
+	}
 	
 }
